159dc5594df6c7d2f59231a43611b6308dcaed88
lucas.llf2608 <lucas.llf2608@gmail.com>
2025-04-11 08:44:07 -0300

    Project created

---
+++ src/main/java/com/bootcamp/springjpa/bootcamp_sj/BootcampSjApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.springjpa.bootcamp_sj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/springjpa/bootcamp_sj/BootcampSjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.springjpa.bootcamp_sj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
