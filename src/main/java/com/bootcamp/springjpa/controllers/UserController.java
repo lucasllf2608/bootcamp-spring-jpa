a9341acd5569429cf8c991ebbac2a7ec7c86a122
lucas.llf2608 <lucas.llf2608@gmail.com>
2025-04-18 14:22:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/bootcamp/springjpa/controllers/UserController.java
@@ -0,0 +1,38 @@
+package com.bootcamp.springjpa.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bootcamp.springjpa.Entities.User;
+import com.bootcamp.springjpa.services.UserService;
+
+
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		
+		List<User> listUsers = userService.findAll();
+		return ResponseEntity.ok().body(listUsers);
+	
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findByID(@PathVariable Long id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
+	} 
+	
+}
---
+++ src/main/java/com/bootcamp/springjpa/services/UserService.java
@@ -0,0 +1,27 @@
+package com.bootcamp.springjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bootcamp.springjpa.Entities.User;
+import com.bootcamp.springjpa.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
+	}
+	
+}
