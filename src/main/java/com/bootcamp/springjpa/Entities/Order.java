1575975a2c473f13c79f9f70e5fe651be9bd369c
lucas.llf2608 <lucas.llf2608@gmail.com>
2025-05-02 08:21:54 -0300

    Payment entity

--- src/main/java/com/bootcamp/springjpa/Entities/Order.java
+++ src/main/java/com/bootcamp/springjpa/Entities/Order.java
@@ -10,6 +10,7 @@ import com.bootcamp.springjpa.Entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -43,6 +45,9 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
@@ -109,6 +114,14 @@ public class Order implements Serializable{
 
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 	
 	
--- src/main/java/com/bootcamp/springjpa/config/TestConfig.java
+++ src/main/java/com/bootcamp/springjpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.bootcamp.springjpa.Entities.Category;
 import com.bootcamp.springjpa.Entities.Order;
 import com.bootcamp.springjpa.Entities.OrderItem;
+import com.bootcamp.springjpa.Entities.Payment;
 import com.bootcamp.springjpa.Entities.Product;
 import com.bootcamp.springjpa.Entities.User;
 import com.bootcamp.springjpa.Entities.enums.OrderStatus;
@@ -76,6 +77,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
